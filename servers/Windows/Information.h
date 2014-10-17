7bafdc12e5e45832a65db3bdec25cfc9a8088da0
Erik <ikas669@gmail.com>
2014-10-18 01:04:36 +0200

    Structure

--- servers/Windows/Information.h
+++ servers/Windows/Information.h
@@ -1,19 +1,23 @@
 #include <windows.h>
 
+#define CONNECTION_IP "127.0.0.1"
+#define CONNECTION_PORT 1234
+
 struct COMPUTER
 {
-    char NAME, RAM, ARCHITECURE;
+    char NAME, RAM
+    char ARCHITECURE[3]; //x86 or x64
 } info_comp;
 
 struct OS
 {
-    char CAPTION, BUILD_NUMBER, MINOR_VERSION, MAJOR_VERSION;
+    char CAPTION /* I.E: Windows 7 */, BUILD_NUMBER, MINOR_VERSION, MAJOR_VERSION;
 } info_os;
 
 struct USER
 {
-    char NAME, COUNTRY;
-    BOOL IS_ADMIN;
+    char NAME, COUNTRY, COUNTRY_SHORT; //COUNTRY = Spain, COUNTRY_SHORT = ES
+    BOOL IS_ADMIN; //Has admin rights
 } info_user;
 
 void GatherInformation();

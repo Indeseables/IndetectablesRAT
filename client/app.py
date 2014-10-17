e1ab60a4a12a370d62031ed0382c1c31da7bc455
sankosk <sankoxh@live.com>
2014-10-18 01:21:07 +0200

    Updating templates because in a near future I will need it

--- client/app.py
+++ client/app.py
@@ -7,6 +7,15 @@ THIS CODE IS IMCOMPLET YET, IM DEVELOPING THE DESIGN OF THE SITE BY THE MOMENT
 @app.route("/")
 def index():
     return render_template("index.html")
+    
 
+@app.route("/pcs")
+def windows_remotes():
+	return render_template("pcs.html")
+
+@app.route("/androids")
+def android_remotes():
+	return render_template("androids.html")
+	
 if __name__ == "__main__":
     app.run(debug=True)
